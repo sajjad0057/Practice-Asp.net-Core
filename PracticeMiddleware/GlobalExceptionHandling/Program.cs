3bc7dba86b353bae76425d66177c883336762042
sajjad0057 <sajjadhossain0057@gmail.com>
2023-05-02 00:39:56 +0600

    Configure Serilog

--- PracticeMiddleware/GlobalExceptionHandling/Program.cs
+++ PracticeMiddleware/GlobalExceptionHandling/Program.cs
@@ -1,6 +1,20 @@
+using Serilog;
+using Serilog.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+#region Serilog(Logger) Configuraion
+
+builder.Host.UseSerilog((ctx, lc) => lc
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(builder.Configuration)
+);
+
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +22,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+Log.Information("Application Starting .");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
