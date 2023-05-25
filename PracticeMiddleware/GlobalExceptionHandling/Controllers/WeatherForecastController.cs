3896dde02b190b8c57820e2d5b08869585fb48f0
sajjad0057 <sajjadhossain0057@gmail.com>
2023-05-25 14:19:56 +0600

    Pratice how to write objects values into log file

--- PracticeMiddleware/GlobalExceptionHandling/Controllers/WeatherForecastController.cs
+++ PracticeMiddleware/GlobalExceptionHandling/Controllers/WeatherForecastController.cs
@@ -21,10 +21,25 @@ namespace GlobalExceptionHandling.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public async Task<IEnumerable<WeatherForecast>> Get()
         {
-            int x = 0;
-            var res = 5 / x;
+            //int x = 0;
+            //var res = 5 / x;
 
             await Task.CompletedTask;
+
+            var obj = new
+            {
+                name = "sajjad",
+                gender = "male",
+                contact = new
+                {
+                    mobile = 01774135869,
+                    mail = "sajjad@gmail.com"
+                },
+                arr  = new[] {1,3,4,5,6,7,8,9,10,11,12}
+            };
+
+            _logger.LogInformation($"obj : {obj}");
+            _logger.LogInformation("obj values :  {@obj} : ", obj);
 
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
