d4532ddaa2911a39d9bcd947a4416078a2842fa8
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-03-06 01:58:54 +0600

    Add employee controller and model

---
+++ PracticeMiddleware/GlobalExceptionHandling/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using GlobalExceptionHandling.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalExceptionHandling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        /// <summary>
+        /// Create a new employee test purpose
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns>result of the operations</returns>
+        [HttpPost]
+        public IActionResult PostEmployee([FromBody] Employee employee)
+        {
+            return Ok(new { Message = "Employee created successfully", Employee = employee});
+        }
+    }
+}
---
+++ PracticeMiddleware/GlobalExceptionHandling/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GlobalExceptionHandling.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(100, ErrorMessage = "Name must be between 2 and 100 characters", MinimumLength = 2)]
+    public string Name { get; set; }
+
+    [Range(18, 60, ErrorMessage = "Age must be between 18 and 60")]
+    public int Age { get; set; }
+
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    public string Email { get; set; }
+}
